39ba8d389fb15db1ada28df76bf5ed8231809be8
Vlad Shakhno <vladyslav.shakhno@nixsolutions.com>
2021-10-26 19:58:54 +0300

    Add getters, setters and constructors

--- 17_sql_jdbc/src/main/java/com/nixsolutions/crudapp/entity/Role.java
+++ 17_sql_jdbc/src/main/java/com/nixsolutions/crudapp/entity/Role.java
@@ -1,8 +1,44 @@
 package com.nixsolutions.crudapp.entity;
 
+import java.util.Objects;
+
 public class Role {
 
-    Long id;
+    private Long id;
 
-    String name;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(java.lang.Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role(String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role() {
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" + "id=" + id + ", name='" + name + '\'' + '}';
+    }
 }
--- 17_sql_jdbc/src/main/java/com/nixsolutions/crudapp/entity/User.java
+++ 17_sql_jdbc/src/main/java/com/nixsolutions/crudapp/entity/User.java
@@ -4,17 +4,117 @@ import java.util.Date;
 
 public class User {
 
-    Long id;
+    private Long id;
 
-    String login;
+    private String login;
 
-    String password;
+    private String password;
 
-    String email;
+    private String email;
 
-    String firstName;
+    private String firstName;
 
-    String LastName;
+    private String lastName;
 
-    Date birthday;
+    private Date birthday;
+
+    private Long roleId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long aRole) {
+        this.roleId = aRole;
+    }
+
+    public User(Long id, String login, String password, String email,
+            String firstName, String lastName, Date birthday, Long roleId) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+        this.roleId = roleId;
+    }
+
+    public User(String login, String password, String email, String firstName,
+            String lastName, Date birthday, Long role) {
+        this.login = login;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+        this.roleId = role;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", login='" + login + '\''
+                + ", password='" + password + '\'' + ", email='" + email + '\''
+                + ", firstName='" + firstName + '\'' + ", lastName='" + lastName
+                + '\'' + ", birthday=" + birthday + ", roleId=" + roleId + '}';
+    }
 }
