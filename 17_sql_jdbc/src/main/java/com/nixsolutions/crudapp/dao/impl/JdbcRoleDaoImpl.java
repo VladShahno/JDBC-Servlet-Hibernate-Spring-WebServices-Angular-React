779a7af5c55f9a325305d2a9f5d7a0c065546c71
Vlad Shakhno <vladyslav.shakhno@nixsolutions.com>
2021-10-26 20:02:02 +0300

    Create impl classes for DAO layer

--- 17_sql_jdbc/src/main/java/com/nixsolutions/crudapp/dao/impl/JdbcRoleDaoImpl.java
+++ 17_sql_jdbc/src/main/java/com/nixsolutions/crudapp/dao/impl/JdbcRoleDaoImpl.java
@@ -1,4 +1,178 @@
 package com.nixsolutions.crudapp.dao.impl;
 
-public class JdbcRoleDaoImpl {
+import com.nixsolutions.crudapp.dao.RoleDao;
+import com.nixsolutions.crudapp.db.ConnectionInitializer;
+import com.nixsolutions.crudapp.entity.Role;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcRoleDaoImpl implements RoleDao {
+
+    private static final String INSERT_ROLE_SQL =
+            "INSERT INTO ROLES" + "(name) VALUES " + " (?);";
+
+    private static final String SELECT_ROLE_BY_ID = "SELECT id, name FROM ROLES where id =?";
+
+    private static final String SELECT_ROLE_BY_NAME = "SELECT id, name FROM ROLES where name =?";
+
+    private static final String UPDATE_ROLE = "UPDATE ROLES set name =? where id =?;";
+
+    private static final String SELECT_ALL_USERS = "SELECT * FROM ROLES";
+
+    private static final String REMOVE_ROLE_SQL = "DELETE FROM ROLES where id = ?;";
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+
+    @Override
+    public void create(Role role) throws SQLException {
+        try {
+
+            DataSource dataSource = ConnectionInitializer.getDataSource();
+            connection = dataSource.getConnection();
+            connection.setAutoCommit(false);
+            preparedStatement = connection.prepareStatement(INSERT_ROLE_SQL);
+
+            preparedStatement.setString(1, role.getName());
+
+            preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public void update(Role role) throws SQLException {
+
+        try {
+            DataSource dataSource = ConnectionInitializer.getDataSource();
+            connection = dataSource.getConnection();
+            connection.setAutoCommit(false);
+            preparedStatement = connection.prepareStatement(UPDATE_ROLE);
+
+            preparedStatement.setString(1, role.getName());
+            preparedStatement.setLong(2, role.getId());
+
+            preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public void remove(Role role) throws SQLException {
+
+        try {
+            DataSource dataSource = ConnectionInitializer.getDataSource();
+            connection = dataSource.getConnection();
+            connection.setAutoCommit(false);
+            preparedStatement = connection.prepareStatement(REMOVE_ROLE_SQL);
+
+            preparedStatement.setLong(1, role.getId());
+
+            preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public List<Role> findAll() {
+
+        List<Role> roles = new ArrayList<>();
+        try {
+            DataSource dataSource = ConnectionInitializer.getDataSource();
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                roles.add(new Role(id, name));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return roles;
+    }
+
+    @Override
+    public Role findById(Long id) {
+
+        Role role = null;
+        try {
+            DataSource dataSource = ConnectionInitializer.getDataSource();
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(SELECT_ROLE_BY_ID);
+
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                String name = resultSet.getString("name");
+                role = new Role(id, name);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return role;
+    }
+
+    @Override
+    public Role findByName(String name) {
+
+        Role role = null;
+        try {
+            DataSource dataSource = ConnectionInitializer.getDataSource();
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    SELECT_ROLE_BY_NAME);
+
+            preparedStatement.setString(1, name);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                role = new Role(id, name);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return role;
+    }
 }
--- 17_sql_jdbc/src/main/java/com/nixsolutions/crudapp/dao/impl/JdbcUserDaoImpl.java
+++ 17_sql_jdbc/src/main/java/com/nixsolutions/crudapp/dao/impl/JdbcUserDaoImpl.java
@@ -1,4 +1,117 @@
 package com.nixsolutions.crudapp.dao.impl;
 
-public class JdbcUserDaoImpl {
+import com.nixsolutions.crudapp.dao.UserDao;
+import com.nixsolutions.crudapp.db.ConnectionInitializer;
+import com.nixsolutions.crudapp.entity.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.List;
+
+public class JdbcUserDaoImpl implements UserDao {
+
+    private static final String INSERT_USERS_SQL = "INSERT INTO USERS"
+            + "  (login, password, email, firstName, lastName, birthday, role_id) VALUES "
+            + " (?, ?, ?, ?, ?, ?, ?);";
+
+    private static final String SELECT_USER_BY_EMAIL = "SELECT id, login, password, email, firstName, lastName, birthday, role_id FROM USERS WHERE email =?";
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+
+    @Override
+    public void create(User user) throws SQLException {
+        try {
+            DataSource dataSource = ConnectionInitializer.getDataSource();
+            connection = dataSource.getConnection();
+            connection.setAutoCommit(false);
+            preparedStatement = connection.prepareStatement(INSERT_USERS_SQL);
+
+            preparedStatement.setString(1, user.getLogin());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setString(4, user.getFirstName());
+            preparedStatement.setString(5, user.getLastName());
+            preparedStatement.setDate(6, (Date) user.getBirthday());
+            preparedStatement.setLong(7, user.getRoleId());
+
+            preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void remove(User user) {
+
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User findByLogin(String login) {
+        return null;
+    }
+
+    @Override
+    public User findByEmail(String email) {
+
+        User user = null;
+        try {
+            DataSource dataSource = ConnectionInitializer.getDataSource();
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    SELECT_USER_BY_EMAIL);
+
+            preparedStatement.setString(1, email);
+
+            System.out.println("fd");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String login = resultSet.getString("login");
+                String password = resultSet.getString("password");
+                email = resultSet.getString("email");
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                Date birthday = resultSet.getDate("birthday");
+                Long role_id = resultSet.getLong("role_id");
+
+                user = new User(id, login, password, email, firstName, lastName,
+                        null, role_id);
+            }
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return user;
+    }
 }
