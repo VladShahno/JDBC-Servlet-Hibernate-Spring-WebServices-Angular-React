efc8218797ede527f83ae74d638fc2657c7d7bff
Vlad Shakhno <vladyslav.shakhno@nixsolutions.com>
2021-10-28 09:53:52 +0300

    create AbstractJdbcDao for connection

---
+++ 17_sql_jdbc/src/main/java/com/nixsolutions/crudapp/dao/AbstractJdbcDao.java
@@ -0,0 +1,12 @@
+package com.nixsolutions.crudapp.dao;
+
+import com.nixsolutions.crudapp.util.DataSourceUtil;
+
+import java.sql.Connection;
+
+public class AbstractJdbcDao {
+
+    public Connection createConnection() {
+        return new DataSourceUtil().getConnection();
+    }
+}
