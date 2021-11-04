6852c6c9351e60376f19f9e357cac70066497e7f
Vlad Shakhno <vladyslav.shakhno@nixsolutions.com>
2021-11-04 19:43:45 +0200

    update pom, properties; add DDL script and Docker file

--- 25_docker/src/main/java/com/nixsolutions/crudapp/MainApp.java
+++ 25_docker/src/main/java/com/nixsolutions/crudapp/MainApp.java
@@ -1,6 +1,20 @@
 package com.nixsolutions.crudapp;
 
+import com.nixsolutions.crudapp.dao.RoleDao;
+import com.nixsolutions.crudapp.dao.UserDao;
+import com.nixsolutions.crudapp.dao.impl.JdbcRoleDaoImpl;
+import com.nixsolutions.crudapp.dao.impl.JdbcUserDaoImpl;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class MainApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, SQLException {
+
+        UserDao userDao = new JdbcUserDaoImpl();
+        System.out.println(userDao.findAll());
+        RoleDao roleDao = new JdbcRoleDaoImpl();
+        System.out.println(roleDao.findByName("ADMIN"));
+        System.out.println(roleDao.findByName("USER"));
     }
 }
